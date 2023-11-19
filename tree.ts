6fa3a1558ae0491d9537e4d2da90d089ba029f4d
ManavDaggula <manavdaggula@gmail.com>
2023-11-19 20:45:21 +0530

    init commit

---
+++ tree.ts
@@ -0,0 +1,128 @@
+class TreeNode {
+  data: Number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(data: Number) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+  inorderTraversal(): Number[] {
+    let arr: Number[] = [];
+    if (this.left) {
+      arr = [...this.left.inorderTraversal()];
+    }
+    arr = [...arr, this.data];
+    if (this.right) {
+      arr = [...arr, ...this.right.inorderTraversal()];
+    }
+    return arr;
+  }
+  preorderTraversal(): Number[] {
+    let arr: Number[] = [];
+    arr = [this.data];
+    if (this.left) {
+      arr = [...arr, ...this.left.preorderTraversal()];
+    }
+    if (this.right) {
+      arr = [...arr, ...this.right.preorderTraversal()];
+    }
+    return arr;
+  }
+  postorderTraversal(): Number[] {
+    let arr: Number[] = [];
+    if (this.left) {
+      arr = [...this.left.postorderTraversal()];
+    }
+    if (this.right) {
+      arr = [...arr, ...this.right.postorderTraversal()];
+    }
+    arr = [...arr, this.data];
+    return arr;
+  }
+}
+
+class BinarySearchTree {
+  root: TreeNode | null;
+
+  isEmpty(): boolean {
+    return this.root ? false : true;
+  }
+
+  addNode(data: Number) {
+    if (this.root) {
+      let ptr = this.root;
+      while (true) {
+        if (data < ptr.data) {
+          if (ptr.left) {
+            ptr = ptr.left;
+          } else {
+            break;
+          }
+        } else {
+          if (ptr.right) {
+            ptr = ptr.right;
+          } else {
+            break;
+          }
+        }
+      }
+      if (data < ptr.data) {
+        ptr.left = new TreeNode(data);
+      } else {
+        ptr.right = new TreeNode(data);
+      }
+    } else {
+      this.root = new TreeNode(data);
+    }
+  }
+
+  searchNode(data: Number) : TreeNode|void {
+    if(this.root){
+        let ptr : TreeNode|null= this.root;
+        while(ptr?.data != data && ptr!=null){
+            if(data < ptr.data){
+              ptr = ptr.left;
+            }
+            else{
+              ptr = ptr.right;
+            }
+        }
+        if(ptr?.data == data){
+          return ptr;
+        }
+    }
+      return;
+  }
+  
+  deleteNode(data: Number) {
+    
+  }
+
+  inorderTraversal() {
+    return this.root ? this.root.inorderTraversal() : [];
+  }
+
+  preorderTraversal() {
+    return this.root ? this.root.preorderTraversal() : [];
+  }
+
+  postorderTraversal() {
+    return this.root ? this.root.postorderTraversal() : [];
+  }
+}
+
+function main(): void {
+  let bst = new BinarySearchTree();
+  bst.addNode(3);
+  bst.addNode(2);
+  bst.addNode(1);
+  bst.addNode(6);
+  bst.addNode(5);
+  console.log(bst.searchNode(1));
+  console.log(bst.preorderTraversal());
+  console.log(bst.inorderTraversal());
+  console.log(bst.postorderTraversal());
+}
+
+main();
